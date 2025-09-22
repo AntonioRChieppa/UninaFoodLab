b4b5692fbfea5abbf8f5c7b213400cd6103605da
anton <antichiep0708@gmail.com>
2025-09-22 20:14:37 +0200

    Aggiornamento sezione test

--- src/start_test.java
+++ src/start_test.java
@@ -3,6 +3,7 @@ import dto.ChefDTO;
 import exception.ChefOperationException;
 import exception.ChefNotFoundException;
 import exception.InvalidCredentialsException;
+import exception.ChefAlreadyExistsException;
 import java.util.*;
 
 public class start_test {
@@ -12,22 +13,31 @@ public class start_test {
 
         try {
         	
+        	controller.registrazioneChef("Mario", "De Maio", "mdm@email.com", "Password@123");
         	controller.loginChef("mdm@email.com", "Password@123");
+        	System.out.println("Login effettuato con successo!");
+        	ChefDTO mario = controller.visualizzaChef();
+        	System.out.println("ID: "+mario.getId()+"Nome: "+mario.getNome()+" Cognome: "+mario.getCognome()+" Email: "+mario.getEmail());
         	controller.eliminaChef();
+        	System.out.println("Chef eliminato con successo!\n");
+        	System.out.println("--------------------------------------\n");
         	
+        	controller.registrazioneChef("Antonio", "Chieppa", "antoniochieppa04@gmail.com", "Aurorina_2013");
         	controller.loginChef("antoniochieppa04@gmail.com", "Aurorina_2013");
-        	controller.eliminaChef();
+        	controller.aggiornaChefPassword("Password@123");
         	System.out.println("\n------------- Visualizzazione tutti gli chef ----------------\n");
         	List<ChefDTO> list = controller.visualizzaTuttiChef();
         	for(ChefDTO c: list) {
-        		System.out.println("ID: "+c.getId()+" Nome: "+c.getNome()+" Cognome: "+c.getCognome());
+        		System.out.println("ID: "+c.getId()+" Nome: "+c.getNome()+" Cognome: "+c.getCognome()+"Password: "+c.getPassword());
         	}
-        	
+        	controller.eliminaChef();
         
         	
-        } catch(InvalidCredentialsException e) {
+        }catch(ChefAlreadyExistsException e) {
         	System.err.println("Errore: "+e.getMessage());
-        } catch(ChefNotFoundException e) {
+        }catch(InvalidCredentialsException e) {
+        	System.err.println("Errore: "+e.getMessage());
+        }catch(ChefNotFoundException e) {
         	System.err.println("Errore: "+e.getMessage());
         }catch (ChefOperationException e) {
             System.err.println("Errore di operazione: " + e.getMessage());
