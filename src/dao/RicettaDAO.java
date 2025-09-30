dbfee102c39b1557c8225c02ed0a8f53cf15a9d4
gabri <gabri@Gabriele.homenet.telecomitalia.it>
2025-10-01 00:08:07 +0200

    Creazione nella classe RicettaDAO di updateRicetta e getRicettaByName

--- src/dao/RicettaDAO.java
+++ src/dao/RicettaDAO.java
@@ -9,18 +9,56 @@ import java.util.*;
 public class RicettaDAO {
 	
 	//Insert newRicetta
-	public void insertRicetta(RicettaDTO newricetta) throws SQLException{
+	public void insertRicetta(RicettaDTO newRicetta) throws SQLException{
 		String sql = "INSERT INTO ricetta (nomericetta, tempopreparazione, porzioni, difficolta VALUES (?,?,?,?)" ;
 		
 		try(Connection conn = db_connection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)){
-			ps.setString(1, newricetta.getNomeRicetta());
-			ps.setInt(2, newricetta.getTempoPreparazione());
-			ps.setInt(3, newricetta.getPorzioni());
-			ps.setString(4, newricetta.getDifficolta());
+			ps.setString (1, newRicetta.getNomeRicetta());
+			ps.setInt (2, newRicetta.getTempoPreparazione());
+			ps.setInt (3, newRicetta.getPorzioni());
+			ps.setString (4, newRicetta.getDifficolta());
 			ps.executeUpdate();
 		}
 	}
 	
+	//UPDATE newRicetta - LOGICA COALESCE
+	public void updateRicetta(RicettaDTO newRicetta) throws SQLException{
+		String sql = "UPDATE ricetta SET "
+				+"nomericetta = COALESCE(?,nomericetta)," 
+				+"tempoPreparazione = COALESCE(?,tempoPreparazione),"
+				+"porzioni =  COALESCE(?,porzioni),"
+				+"difficolta = COALESCE(?,difficolta)"
+				+"WHERE idricetta = ?";
+		try(Connection conn = db_connection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)){
+			ps.setString (1, newRicetta.getNomeRicetta());
+			ps.setInt (2, newRicetta.getTempoPreparazione());
+			ps.setInt (3, newRicetta.getPorzioni());
+			ps.setString(4, newRicetta.getDifficolta());
+			ps.executeUpdate();
+		}
+	}
+	//READ (SELECT) Ricetta by nome
 	
-	
-}
+	public RicettaDTO getRicettaByName(String nomeRicetta) throws SQLException {
+		String sql = "SELECT * FROM ricetta WHERE nomeRicetta = ?";
+		
+		try(Connection conn = db_connection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)){
+			ps.setString(1, nomeRicetta);
+			ResultSet rs = ps.executeQuery(); 
+			
+			if(rs.next()) {
+				RicettaDTO ricetta = new RicettaDTO();
+				ricetta.setId(rs.getInt("idRicetta"));
+				ricetta.setNomeRicetta(rs.getString("nomeRicetta"));
+				ricetta.setTempoPreparazione(rs.getInt("tempoPreparazione"));
+				ricetta.setPorzioni(rs.getInt("porzioni"));
+				ricetta.setDifficolta(rs.getString("difficolta"));
+				return ricetta;
+			}
+			else {
+				return null;
+					
+				}
+			}
+		}
+	}
