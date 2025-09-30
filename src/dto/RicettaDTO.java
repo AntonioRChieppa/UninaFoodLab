285c030a84f82c8ee55356c585cc71110acb3a1d
gabri <gabri@Gabriele.homenet.telecomitalia.it>
2025-09-30 21:46:54 +0200

    Creazione RicettaDTO, appena finita la classe sessione eliminare i commenti in ricetta per collegamento chiave esterna

---
+++ src/dto/RicettaDTO.java
@@ -0,0 +1,81 @@
+package dto;
+
+public class RicettaDTO {
+
+	//ATTRIBUTI
+	private int id;
+	private String nomeRicetta;
+	private int tempoPreparazione;
+	private int porzioni;
+	private String difficolta;
+	//private SessioneDTO sessioneRicetta;
+
+	/* DA AGGIUNGERE IN SEGUITO ALLA CREAZIONE DI SESSIONE
+	private SessioneDTO sessioneRicetta;
+	*/ 
+	
+	//COSTRUTTORE DI DEFAULT
+	public RicettaDTO () {
+		
+	}
+	
+	//COSTRUTTORE SPECIFICO
+	public RicettaDTO(int id, String nomeRicetta, int tempoPreparazione, int porzioni, String difficolta/*,SessioneDTO sessioneRicetta*/) {
+		this.id = id;
+		this.nomeRicetta = nomeRicetta;
+		this.tempoPreparazione = tempoPreparazione;
+		this.porzioni = porzioni;
+		this.difficolta = difficolta;
+		//this.sessioneRicetta = sessioneRicetta;
+	}
+	
+	//GETTER E SETTER
+	
+	public int getID() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNomeRicetta() {
+		return nomeRicetta;
+	}
+	
+	public void setNomeRicetta(String nomeRicetta) {
+		this.nomeRicetta = nomeRicetta;
+	}
+	
+	public int getTempoPreparazione() {
+		return tempoPreparazione;
+	}
+	
+	public void setTempoPreparazione(int tempoPreparazione) {
+		this.tempoPreparazione = tempoPreparazione;
+	}
+	
+	public int getPorzioni() {
+		return porzioni;
+	}
+	
+	public void setPorzioni(int porzioni) {
+		this.porzioni = porzioni;
+	}
+	
+	public String getDifficolta() {
+		return difficolta;
+	}
+	
+	public void setDifficolta(String difficolta) {
+		this.difficolta = difficolta;
+	}
+	
+	/*public SessioneDTO getSessioneRicetta(){
+	 	return sessioneRicetta;
+	}
+	
+	public void setSessioneRicetta(SessioneDTO sessioneRicetta){
+		this.sessioneRicetta = sessioneRicetta;
+	} */
+}
